914dd3f5993152437daf2b76ca764cb2c5c5aaa6
LauraBennett <lbenn@us.ibm.com>
2017-11-18 16:26:46 -0800

    fixing CI errors

--- CognitiveConcierge-Server/Package.swift
+++ CognitiveConcierge-Server/Package.swift
@@ -1,12 +1,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "restaurant-recommendations",
+    name: "CognitiveConcierge",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 2),
-        .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 3, minor: 0)
-        .Package(url: "https://github.com/IBM/metrics-tracker-client-swift.git", majorVersion: 5),
+        .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 3, minor: 0),
+        .Package(url: "https://github.com/IBM/metrics-tracker-client-swift.git", majorVersion: 5)
         ]
 )
