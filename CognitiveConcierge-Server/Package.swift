14d330316cd2534a02e55dde11e8c26c19665acd
LauraBennett <lbenn@us.ibm.com>
2017-11-29 08:35:26 -0800

    fixing build error

--- CognitiveConcierge-Server/Package.swift
+++ CognitiveConcierge-Server/Package.swift
@@ -1,8 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "restaurant-recommendations
-    ",
+    name: "restaurant-recommendations",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
