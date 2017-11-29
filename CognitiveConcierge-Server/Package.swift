aa0c0e89b8ecd98cd9fb64f06f2d259ba03a3497
LauraBennett <lbenn@us.ibm.com>
2017-11-28 16:35:45 -0800

    updating package.swift

--- CognitiveConcierge-Server/Package.swift
+++ CognitiveConcierge-Server/Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "restaurant-recommendations",
+    name: "CognitiveConcierge",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
