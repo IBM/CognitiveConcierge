29592b005bdc4b2ee7bc84a715b99e24a2796e75
Steve Martinelli <stevemart@users.noreply.github.com>
2018-05-16 15:31:59 -0400

    remove metrics tracker dependency (#51)

--- CognitiveConcierge-Server/Package.swift
+++ CognitiveConcierge-Server/Package.swift
@@ -12,13 +12,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.1.0")),
         .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/IBM/metrics-tracker-client-swift.git", .upToNextMinor(from: "5.0.0")),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", .upToNextMajor(from: "17.0.0"))
 	],
   targets: [
     .target(
       name: "CognitiveConcierge",
-      dependencies: ["Kitura", "SwiftyJSON", "CloudEnvironment", "MetricsTrackerClient"],
+      dependencies: ["Kitura", "SwiftyJSON", "CloudEnvironment"],
       path: "."
     )
   ]
